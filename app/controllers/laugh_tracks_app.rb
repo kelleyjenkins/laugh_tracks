a8ee3db4a5577f7a621338ec0ff77742e8018fd7
kelleyjenkins <ckjenkins3@gmail.com>
2017-12-01 10:51:32 -0700

    trying to add specials to comedians

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,10 +3,11 @@ class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
     @comedians = Comedian.all
+    @specials = Special.all
     erb :index
   end
 
-  
+
 
 
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,7 @@
 class Comedian < ActiveRecord::Base
+  belongs_to :special
 
+  def self.average_age
+    average(:age)
+  end
 end
