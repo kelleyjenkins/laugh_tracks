40a881f50ba00419238aaf830858135c4bbbbc66
kelleyjenkins <ckjenkins3@gmail.com>
2017-12-01 10:23:34 -0700

    add index erb code

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,9 +1,12 @@
 class LaughTracksApp < Sinatra::Base
-  set root, File.expand_path("..", __dir__)
+  set :root, File.expand_path("..", __dir__)
 
   get '/comedians' do
+    @comedians = Comedian.all
     erb :index
   end
 
   
+
+
 end
